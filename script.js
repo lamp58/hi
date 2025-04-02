dd6e25ba71c31813b60a465b9c45c2f1e6867f61
lamp58 <159870258+lamp58@users.noreply.github.com>
2025-04-02 10:36:27 +0545

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const yesBtn = document.querySelector(".yes-btn");
 const noBtn = document.querySelector(".no-btn");
 
 yesBtn.addEventListener("click", () => {
-  question.innerHTML = "happy anniversary baby<3!";
+  question.innerHTML = "YaY!";
   gif.src =
     "https://media.giphy.com/media/UMon0fuimoAN9ueUNP/giphy.gif";
 });
